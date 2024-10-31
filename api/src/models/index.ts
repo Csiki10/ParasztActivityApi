f8a59ef89c3d601c9ba338a2eec5b1f0c6fcb638
benedek.csikos <csikosbenedek@gmail.com>
2024-10-31 17:48:19 +0100

    init

--- api/src/models/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './game-item.model';
-export * from './game.model';
-export * from './item.model';
-export * from './round.model';
-export * from './team.model';
--- api/src/repositories/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './game-item.repository';
-export * from './game.repository';
-export * from './item.repository';
-export * from './round.repository';
-export * from './team.repository';
